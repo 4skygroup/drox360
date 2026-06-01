9ece633ac979c348e56c5441972085c4b82f8715
Antoine-Chen <antoinecoc2408@gmail.com>
2026-06-01 11:30:48 +0200

    ajout des packages + routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import {Route, Routes} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop.tsx";
+import Home from "./pages/Home.tsx";
 
 function App() {
   return (
     <>
-        <p className="text-h5 bg-drox360-orange">
-          bonjour
-        </p>
+        <ScrollToTop />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            {/*<Route path="/services" element={<Services />} />*/}
+            {/*<Route path="/about" element={<About/> } />*/}
+            {/*<Route path="/contact" element={<Contact />} />*/}
 
+        </Routes>
     </>
   )
 }
