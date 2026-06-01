abb3c13dc93a85654c7360876b95e4eea42722c4
Antoine-Chen <antoinecoc2408@gmail.com>
2026-06-01 11:57:17 +0200

    ajout de la navbar et du footer + toutes les routes l'app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import {Route, Routes} from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop.tsx";
 import Home from "./pages/Home.tsx";
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/footer/Footer.tsx";
 
 function App() {
   return (
     <>
         <ScrollToTop />
+        <Navbar/>
         <Routes>
             <Route path="/" element={<Home />} />
             {/*<Route path="/services" element={<Services />} />*/}
             {/*<Route path="/about" element={<About/> } />*/}
             {/*<Route path="/contact" element={<Contact />} />*/}
 
+            {/*<Route path="/brandcontent" element={<BrandContent />} />*/}
+            {/*<Route path="/production" element={<Production />} />*/}
+            {/*<Route path="/strategiecreative" element={<StrategieCreative />} />*/}
+            {/*<Route path="/socialmedia" element={<SocialMedia />} />*/}
         </Routes>
+        <Footer/>
     </>
   )
 }
